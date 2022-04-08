f5d5a8bb4e551e9449643c5aff60943ae3e5869a
georgehans1 <georgehans301@gmail.com>
2022-04-08 11:56:23 +0000

    Added notes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { NotesComponent } from './components/pages/notes/notes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
