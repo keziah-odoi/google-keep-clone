0f299a9cff9222f8b9ceb85c09c4afc3325c61eb
georgehans1 <georgehans301@gmail.com>
2022-04-14 15:04:26 +0000

    Added addNote function to notes service

--- src/app/services/notes/notes.service.ts
+++ src/app/services/notes/notes.service.ts
@@ -12,4 +12,8 @@ export class NotesService {
   getNotes(userId:string) {
     return this.httpClient.get(`https://notes-service-app.herokuapp.com/notes/get/${userId}`)
   }
+
+  addNote(payload:any){
+    return this.httpClient.post(`https://notes-service-app.herokuapp.com/notes/add`,payload)
+  }
 }
